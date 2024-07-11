c13575c5f61b7cb522481d109e86a10622efa6da
Olov <johansson.olov@gmail.com>
2024-07-11 12:31:19 +0200

    fixed nurgle

--- src/components/Warscroll.jsx
+++ src/components/Warscroll.jsx
@@ -4,13 +4,31 @@ import WarscrollKeywords from "./WarscrollKeywords";
 import WarscrollWeapons from "./WarscrollWeapons";
 
 function Warscroll({ unit, icon, color }) {
+  const classes = unit.primaryKeywords
+    .map(
+      (item) =>
+        "ws-" +
+        item
+          .toLowerCase()
+          .replace(/\([^)]*\)/g, "")
+          .trim(),
+    )
+    .join(" ");
 
-    return <div className="warscroll" style={{ '--faction-color': `${color}` }}>
-        <WarscrollHeader unit={unit} color={color} />
-        <WarscrollWeapons unit={unit} color={color} />
-        <WarscrollAbilities unit={unit} />
-        <WarscrollKeywords color={color} icon={icon} primaryKeywords={unit.primaryKeywords} secondaryKeywords={unit.secondaryKeywords} />
+  return (
+    <div className={classes} style={{ "--faction-color": `${color}` }}>
+      <WarscrollHeader unit={unit} color={color} />
+      <WarscrollWeapons unit={unit} color={color} />
+      <WarscrollAbilities unit={unit} />
+      <WarscrollKeywords
+        color={color}
+        icon={icon}
+        primaryKeywords={unit.primaryKeywords}
+        secondaryKeywords={unit.secondaryKeywords}
+      />
     </div>
+  );
 }
 
 export default Warscroll;
+
